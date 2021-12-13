5b06d7f0fe9387dccfecc5839c7b1c39d9d7ffaf
DavidALevy <david.levy.levy@btinternet.com>
2021-12-13 15:27:53 +0000

    first commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+require ("./db/connection")
 const express = require("express")
+const cors=require("cors");
+const userRouter=require("./user/userRoutes");
 const app=express();
 const port=5000;
-app.use("/static",express.static("public"));
+app.use(express.json());
+app.use(cors());
+app.use(userRouter);
+//app.use("/home",express.static("public"));
+// app.use("/about",express.static("public/about"));
+// app.use("/contact",express.static("public/contact"));
 app.listen(port,()=>{
     console.log("Listening on port 5000");
 });
